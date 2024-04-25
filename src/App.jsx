6557649570ee3348ad124c6827155db32a2f1b76
stgeary <scottmgeary17@gmail.com>
2024-04-25 13:13:35 -0500

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Content from "./Content";
 import Header from "./Header";
 import Footer from "./Footer";
+import LoginModal from "./LoginModal";
 
 function App() {
   return (
